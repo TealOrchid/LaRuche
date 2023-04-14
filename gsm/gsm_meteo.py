cd18e4be9ed19aebc7df0e2fff7c61d5426cdbb2
TealOrchid <tealorchid@proton.me>
2023-04-14 20:44:48 +0200

    Scripts liés au GSM

---
+++ gsm/gsm_meteo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+
+# import MySQLdb module
+import MySQLdb
+import lib_gsm
+
+# # Connectez-vous à MySQL en utilisant les paramètres spécifiés (chacun d'eux est facultatif)
+connection = MySQLdb.connect(host="localhost", port=3306, user="admin", passwd="Miel", db="ruche", charset="utf8mb4")
+
+# ouvrir cursor
+cursor = connection.cursor()
+
+# Exécuter la requête MySQL
+# Peut utiliser le Format String
+# Aussi les paramètres sont utilisées automatiquement par la bibliothèque en utilisant mysql_real_escape_string
+# Ou si ce n'est pas supporté par MySQL puis mysql_escape_string
+cursor.execute("SELECT thermometrie, hygrometrie, barometrie FROM capteur_meteorologique WHERE heure = (select max(heure) from capteur_meteorologique) and date = (select max(date) from capteur_meteorologique)")
+
+#g=cursor.fetchall()#recuperation de l'ensemble tuple
+#print(g)
+# Obtenir la description de la requête, p.ex. Noms de colonnes, etc.
+description = cursor.description
+
+column_string = ""
+i=0
+# afficher colonnes noms
+for column in description:
+    if i<2:
+        column_string+=column[0]+" | "
+    elif i<3:
+        column_string+=column[0]+"    | "
+    else:
+        column_string+=column[0]+"       | "   
+    i+=1
+    
+   
+print("\n | "+column_string)
+for i in range(cursor.rowcount):
+    row=cursor.fetchone()
+    print(" | {0} | {1} | {2} |".format(row[0],row[1],row[2]))
+    
+#envoie du message
+if row[0] <= 15:
+    #lib_gsm.setup()
+    #lib_gsm.Initialize()
+    #lib_gsm.CheckNetworkStatus()
+    rep=lib_gsm.SendTextMessage("0760291070","Temperature dans la ruche trop basse !")
+    print('EnvoiSMS=',rep,'\n')
+    
+if row[0] >= 40:
+    #lib_gsm.setup()
+    #lib_gsm.Initialize()
+    #lib_gsm.CheckNetworkStatus()
+    rep=lib_gsm.SendTextMessage("0760291070","Temperature dans la ruche trop haute !")
+    print('EnvoiSMS=',rep,'\n')
+    
+if row[1] <= "valeur":
+    #lib_gsm.setup()
+    #lib_gsm.Initialize()
+    #lib_gsm.CheckNetworkStatus()
+    rep=lib_gsm.SendTextMessage("0760291070","Humiditee dans la ruche trop basse !")
+    print('EnvoiSMS=',rep,'\n')
+    
+if row[1] >= "valeur":
+    #lib_gsm.setup()
+    #lib_gsm.Initialize()
+    #lib_gsm.CheckNetworkStatus()
+    rep=lib_gsm.SendTextMessage("0760291070","Humiditee dans la ruche trop elevee !")
+    print('EnvoiSMS=',rep,'\n')
+    
+if row[2] <= "valeur":
+    #lib_gsm.setup()
+    #lib_gsm.Initialize()
+    #lib_gsm.CheckNetworkStatus()
+    rep=lib_gsm.SendTextMessage("0760291070","Pression dans la ruche trop basse !")
+    print('EnvoiSMS=',rep,'\n')
+    
+if row[2] >= "valeur":
+    #lib_gsm.setup()
+    #lib_gsm.Initialize()
+    #lib_gsm.CheckNetworkStatus()
+    rep=lib_gsm.SendTextMessage("0760291070","Pression dans la ruche trop elevee !")
+    print('EnvoiSMS=',rep,'\n')
+    
+cursor.close()#fermeture cursor
+connection.close #fermeture
+
+
+    
+
+
+
+
+
+
