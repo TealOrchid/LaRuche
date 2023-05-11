8bec59b233e266685ac420ca0ec982d37d4d091e
Maurodragmar <95592033+Maurodragmar@users.noreply.github.com>
2023-05-11 09:26:21 +0200

    Update insertion_bme.py
    ajout de la doc

--- capteurs/insertion_bme.py
+++ capteurs/insertion_bme.py
@@ -1,30 +1,30 @@
-#Importations
+# Importations
 import bme280
 import MySQLdb
 import datetime
 import time
 
-#Creation des variables
+# Creation des variables
 temperature, pression, humidité = bme280.lireBME280Val()[0], bme280.lireBME280Val()[1], bme280.lireBME280Val()[2]
 date = str(datetime.datetime.now().date())
 heure = f"{datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}"
 
-#Connection a la base de donnees
+# Connection a la base de donnees
 """"""
 connection = MySQLdb.connect(host="localhost", port=3306, user="admin", passwd="Miel", db="ruche", charset="utf8")
 """"""
 
-# ouvrir cursor
+# Ouverture du cursor
 cursor = connection.cursor()#Les curseurs sont créés par la méthode connection.cursor (): ils sont liés à la connexion pendant toute la durée de vie
 
 
-# Exécuter la requête MySQL
+# Exécution de la requête MySQL
 """"""
 cursor.execute(f"INSERT INTO `capteur_meteorologique` (`thermometrie`, `hygrometrie`, `barometrie`, `date`, `heure`) VALUES ('{temperature}', '{pression}', '{humidité}', '{date}', '{heure}');")
 """"""
 
 # display row count 
-connection.commit()#Cette méthode envoie une instruction COMMIT au serveur MySQL, en engageant la transaction en cours
+connection.commit()# Envoie d'une instruction COMMIT au serveur MySQL, en engageant la transaction en cours
 
 cursor.close()#fermeture cursor
 connection.close ()#fermeture connexion
