b8ed8383eae2538ed1e2c26d399bcc3f961986f2
Maurodragmar <95592033+Maurodragmar@users.noreply.github.com>
2023-05-11 09:25:25 +0200

    Update insertion_gps.py
    ajout de la doc

--- capteurs/insertion_gps.py
+++ capteurs/insertion_gps.py
@@ -1,30 +1,30 @@
+# Importations
 import GPS_ARDUINO
 import MySQLdb
 import datetime
 import time
 
+# Creation des variables
 coordonnees = f"{GPS_ARDUINO.dd_lat}, {GPS_ARDUINO.dd_long}"
-print(coordonnees)
-
 date = str(datetime.datetime.now().date())
 heure = f"{datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}"
 
-    
+# Connection a la base de donnees 
 """"""
 connection = MySQLdb.connect(host="localhost", port=3306, user="admin", passwd="Miel", db="ruche", charset="utf8")
 """"""
 
-# ouvrir cursor
+# Overture du cursor
 cursor = connection.cursor()#Les curseurs sont créés par la méthode connection.cursor (): ils sont liés à la connexion pendant toute la durée de vie
 
 
-# Exécuter la requête MySQL
+# Exécution de la requête MySQL
 """"""
 cursor.execute(f"INSERT INTO `gps` (`coordonnees`, `date`, `heure`) VALUES ('{coordonnees}', '{date}', '{heure}');")
 """"""
 
 # display row count 
-connection.commit()#Cette méthode envoie une instruction COMMIT au serveur MySQL, en engageant la transaction en cours
+connection.commit()#Envoie d'une instruction COMMIT au serveur MySQL, en engageant la transaction en cours
 
 cursor.close()#fermeture cursor
 connection.close ()#fermeture connexion
