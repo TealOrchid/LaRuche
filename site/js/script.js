96cce409c9178bd5cf8bc5b1fc39ab378b888ab4
TealOrchid <tealorchid@proton.me>
2023-05-15 19:25:02 +0200

    Implémentation des graphes météo

---
+++ site/js/script.js
@@ -0,0 +1,19 @@
+let overlap, open, close;
+
+function date_heure() {
+    const date = new Date();
+    const tab_jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi","Vendredi", "Samedi"];
+    const tab_mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    let heures = String(date.getHours());
+    let minutes = String(date.getMinutes());
+    document.querySelector("#date").innerHTML = `Aujourd'hui nous sommes le ${tab_jours[date.getDay()]} ${date.getDate()} ${tab_mois[date.getMonth()]} ${date.getFullYear()}.`
+    document.querySelector("#heure").innerHTML = `Il est ${heures.length < 2 ? "0"+heures : heures} h ${minutes.length < 2 ? "0"+minutes : minutes}.`
+}
+
+window.onload = function() {
+    overlap = document.querySelector("#menu");
+    open = document.querySelector("#open");
+    close = document.querySelector("#close");
+    open.addEventListener("click", function() {overlap.style.transform = "translateY(0)";}, false);
+    close.addEventListener("click", function() {overlap.style.transform = "translateY(-110%)";}, false);
+}
--- site/script.js
+++
@@ -1,9 +0,0 @@
-let overlay, open, close;
-
-window.onload = function() {
-  overlay = document.querySelector("#menu");
-  open = document.querySelector("#open");
-  open.addEventListener("click", function() {overlay.style.transform = "translateY(0)";}, false);
-  close = document.querySelector("#close");
-  close.addEventListener("click", function() {overlay.style.transform = "translateY(-110%)";}, false);
-}
