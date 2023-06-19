2efed4d78f4606926ae73cea381eeb1ea0144f5e
TealOrchid <tealorchid@proton.me>
2023-06-19 19:13:31 +0200

    Modification de la librairie GPS

--- capteurs/gps.py
+++ capteurs/gps.py
@@ -1,39 +1,78 @@
 #!/usr/bin/python3
 # -*- coding:UTF-8 -*-
 
-import serial, time
 import RPi.GPIO as GPIO
+import serial
+import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 
-def init_GPIO():
-    GPIO.setup(7,GPIO.OUT)
-    GPIO.output(7, GPIO.LOW)
-    time.sleep(4)
-    GPIO.output(7, GPIO.HIGH)
-    time.sleep(0.01)
-    GPIO.output(7, GPIO.LOW)
-    time.sleep(5)
+class GPS:
+    @staticmethod
+    def __GPIOSetup():
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(7, GPIO.OUT)
+        GPIO.output(7, GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(7, GPIO.HIGH)
+        time.sleep(0.01)
+        GPIO.output(7, GPIO.LOW)
+        time.sleep(5)
 
-setup = serial.Serial(
-    port = '/dev/ttyAMA0',
-    baudrate = 115200,  # vitesse 115200 bauds
-    parity = serial.PARITY_NONE,  # pas de parité
-    stopbits = serial.STOPBITS_ONE,  # un stop bit
-    bytesize = serial.EIGHTBITS,  # format 8 bits
-    timeout = 3)  # timeout 100 ms
+    def __init__(self, port, baudrate):
+        self.__port = port
+        self.__baudrate = baudrate
+        self.__ser = serial.Serial(
+            port=self.__port,
+            baudrate=self.__baudrate,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=3)
+        self.__connect()
+        self.GNSS_status = 0
+        self.Fix_status = 0
+        self.UTC = ''
+        self.Latitude = 0.0
+        self.Longitude = 0.0
+        self.Altitude = 0.0
+        self.Speed = 0.0
+        self.Course = 0.0
+        self.HDOP = 0.0
+        self.PDOP = 0.0
+        self.VDOP = 0.0
+        self.GPS_satellites = 0
+        self.GNSS_satellites = 0
+        self.Signal = 0.0
+        self.__get_coordinates()
 
-def init_gps():
-    reponse = []
-    while (reponse != [b'AT+CGNSPWR=1\r\r\n', b'OK\r\n']):
-        setup.write(b'AT+CGNSPWR=1\r\n')
-        reponse = setup.readlines()
-        if reponse == []:
-            init_GPIO()
+    def __connect(self):
+        data = []
+        while (data != [b'AT+CGNSPWR=1\r\r\n', b'OK\r\n']):
+            self.__ser.write(b'AT+CGNSPWR=1\r\n')
+            data = self.__ser.readlines()
+            print(data)
+            if not data:
+                self.__GPIOSetup()
 
-def lecture_coordonnees():         
-    setup.write(b"AT+CGNSINF\r\n")
-    reponse = setup.readlines()
-    coordonnees = reponse[1].decode().split(',')
-    return coordonnees[3], coordonnees[4]
+    def __get_coordinates(self):
+        self.__ser.write(b"AT+CGNSINF\r\n")
+        rawData = self.__ser.readlines()
+        data = rawData[1].decode().split(',')
+        self.GNSS_status = data[0]
+        self.Fix_status = data[1]
+        self.UTC = data[2]
+        self.Latitude = data[3]
+        self.Longitude = data[4]
+        self.Altitude = data[5]
+        self.Speed = data[6]
+        self.Course = data[7]
+        self.HDOP = data[10]
+        self.PDOP = data[11]
+        self.VDOP = data[12]
+        self.GPS_satellites = data[14]
+        self.GNSS_satellites = data[15]
+        self.Signal = data[18]
+
+    def close(self):
+        self.__ser.close()
--- capteurs/insertion_gps.py
+++ capteurs/insertion_gps.py
@@ -2,12 +2,12 @@
 # -*- coding:UTF-8 -*-
 
 # Importations
-import gps, MySQLdb, datetime
+import MySQLdb, datetime
+from gps import GPS
 
 # Creation des variables
-gps.init_gps()
-tuple_coord = gps.lecture_coordonnees()
-coordonnees = f"{tuple_coord[0]}, {tuple_coord[1]}"
+gps = GPS('/dev/ttyAMA0', 115200)
+gps.close()
 date = str(datetime.datetime.now().date())
 heure = f"{datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}"
 
@@ -18,7 +18,7 @@ connection = MySQLdb.connect(host="localhost", port=3306, user="admin", passwd="
 cursor = connection.cursor()
 
 # Exécution de la requête MySQL
-cursor.execute(f"INSERT INTO `gps` (`coordonnees`, `date`, `heure`) VALUES ('{coordonnees}', '{date}', '{heure}');")
+cursor.execute(f"INSERT INTO `gps` (`coordonnees`, `date`, `heure`) VALUES ('{f'{gps.Latitude}, {gps.Longitude}'}', '{date}', '{heure}');")
 
 # Envoie d'une instruction COMMIT au serveur MySQL, en engageant la transaction en cours
 connection.commit()
